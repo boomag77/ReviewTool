b97e3b74c19a35067353afb196706b2d17e6d136
boomag77 <s.burdakov@gmail.com>
2026-02-25 12:07:50 -0500

    Before moving Initial review logic from MainWindow

---
+++ InitialReviewProcessor.cs
@@ -0,0 +1,17 @@
+﻿using ReviewTool;
+internal sealed class InitialReviewProcessor
+{
+
+    public InitialReviewProcessor()
+    {
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Finish()
+    {
+    }
+}
